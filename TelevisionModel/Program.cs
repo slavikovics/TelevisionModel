6a99a6560a2be8da42ca66e81b67ed0d4b060214
slavikovics <144949100+slavikovics@users.noreply.github.com>
2025-02-13 09:29:30 +0300

    Добавьте файлы проекта.

---
+++ TelevisionModel/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TelevisionModel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
