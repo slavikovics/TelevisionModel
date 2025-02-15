959711948f363c05edd653f2294f1708ddd1ac79
slavikovics <144949100+slavikovics@users.noreply.github.com>
2025-02-15 11:03:19 +0300

    Null checks for RemoteControl

--- TelevisionModel/RemoteControl.cs
+++ TelevisionModel/RemoteControl.cs
@@ -8,23 +8,22 @@ namespace TelevisionModel
 {
     public class RemoteControl
     {
-        public Television PairedTelevision { get; set; }
+        public Television? PairedTelevision { get; set; }
 
         private delegate void PowerSwitchPushed();
         
-        private PowerSwitchPushed _powerSwitchPushed;
+        private PowerSwitchPushed? _powerSwitchPushed;
 
         public void Pair(Television televisionToPair)
         {
-            // TODO: check for null
-            _powerSwitchPushed -= PairedTelevision.PowerSwitchPushed;
+            if (_powerSwitchPushed is not null && PairedTelevision is not null) _powerSwitchPushed -= PairedTelevision.PowerSwitchPushed;
             PairedTelevision = televisionToPair;
             _powerSwitchPushed += PairedTelevision.PowerSwitchPushed;
         }
 
         public void PushPowerSwitch()
         {
-            _powerSwitchPushed();
+            _powerSwitchPushed?.Invoke();
         }
     }
 }
