ca8b5636e9606a3d1c42afcf1ac9e65d8447e790
slavikovics <144949100+slavikovics@users.noreply.github.com>
2025-02-18 02:25:28 +0300

    Adding Tests project to git

---
+++ TelevisionModelTests/LogoUrlsTests.cs
@@ -0,0 +1,15 @@
+﻿using TelevisionModel;
+
+namespace TelevisionModelTests
+{
+    [TestClass]
+    public sealed class LogoUrlsTests
+    {
+        [TestMethod]
+        public void JsonParsingTest()
+        {
+            Television television = new Television(new SoundSystem(12), new Screen(1920, 1080, "IPS", 120, 50));
+            television.FindChannels();
+        }
+    }
+}
